4792235152fd3eb496a0f4aeffd223c45e0deed6
Ivan Svyatykh <zlativansvyat@mail.ru>
2024-10-11 09:15:42 +0500

    добавил атрибуты документы(забыл )

--- src/database/documents/ticket.py
+++ src/database/documents/ticket.py
@@ -1,7 +1,18 @@
-from mongoengine import Document, StringField , ReferenceField
+from mongoengine import (
+    Document,
+    StringField,
+    ReferenceField,
+    FloatField,
+    IntField,
+    BooleanField,
+)
 
 
 class Ticket(Document):
     meta = {"db_alias": "airport", "collection": "tickets"}
-    passenger = ReferenceField("passengers")
-    
+    passenger = ReferenceField("passengers", required=True)
+    fare_conditions = ReferenceField("seat_classes", required=True)
+    flight = ReferenceField("flights", required=True)
+    cost = FloatField(min_value=0, required=True)
+    baggage_weight = IntField(min_value=0, required=True)
+    is_registred = BooleanField(required=True)

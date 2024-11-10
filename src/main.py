5df07e032d29a3d17a79db2d843e68ed2d88bca0
Ivan Svyatykh <zlativansvyat@mail.ru>
2024-11-10 13:15:09 +0500

    убрал лишний файл

--- src/main.py
+++
@@ -1,9 +0,0 @@
-from database.config import DatabaseConfig
-from database.documents.person import Person
-
-
-db_config = DatabaseConfig()
-db_config.start_connection()
-person = Person(name="Ivan", surname="Svyatykh")
-person.save()
-print(db_config)

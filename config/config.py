18be6a1278bef540eb19ea2f47c84ef9e96e56d9
Ivan Svyatykh <zlativansvyat@mail.ru>
2024-11-16 21:18:16 +0500

    добавил env-ы для minio

--- config/config.py
+++ config/config.py
@@ -3,7 +3,8 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-
+MINIO_MONGODB_USERNAME = os.getenv("S3_ADMIN")
+MINIO_PASSWORD = os.getenv("S3_PASSWORD")
 MONGODB_USERNAME = os.getenv("MONGO_INITDB_ROOT_USERNAME")
 MONGODB_PASSWORD = os.getenv("MONGO_INITDB_ROOT_PASSWORD")
 MONGODB_DOMAIN = os.getenv("DOMAIN")

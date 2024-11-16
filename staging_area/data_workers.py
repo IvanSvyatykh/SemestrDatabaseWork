2bd71b939572366d238624e53c3dd0a7d7ee4766
Ivan Svyatykh <zlativansvyat@mail.ru>
2024-11-16 22:21:04 +0500

    начал делать класс для загрузки данных в minio

--- staging_area/data_workers.py
+++ staging_area/data_workers.py
@@ -1,4 +1,6 @@
 from pyspark.sql import SparkSession, DataFrame
+from pandas import DataFrame as PandasDataFrame
+from pyspark.sql.types import StructType
 
 
 class MongoDbExtractractor:
@@ -16,7 +18,7 @@ class MongoDbExtractractor:
     def start_spark_connection(
         self, spark_ip: str, spark_session_name: str
     ) -> None:
-        self.spark = (
+        self.spark: SparkSession = (
             SparkSession.builder.appName(spark_session_name)
             .master(f"spark://{spark_ip}:7077")
             .config("spark.mongodb.input.uri", self.mongodb_uri)
@@ -36,3 +38,40 @@ class MongoDbExtractractor:
             .option("collection", collection_name)
             .load()
         )
+
+
+class MinioDataLoader:
+
+    def __init__(self, minio_ip: str):
+        self.minio_uri = f"http://{minio_ip}:9000"
+
+    def start_spark_connection(
+        self,
+        spark_ip: str,
+        spark_session_name: str,
+        access_key: str,
+        secret_key: str,
+    ) -> None:
+        self.spark: SparkSession = (
+            SparkSession.builder.appName(spark_session_name)
+            .master(f"spark://{spark_ip}:7077")
+            .config("fs.s3a.endpoint", self.minio_uri)
+            .config(
+                "fs.s3a.impl",
+                "org.apache.hadoop.fs.s3a.S3AFileSystem",
+            )
+            .config("fs.s3a.access.key", access_key)
+            .config("fs.s3a.secret.key", secret_key)
+            .getOrCreate()
+        )
+
+    def get_spark_dataframe(
+        self, dataframe: PandasDataFrame, schema: StructType
+    ) -> DataFrame:
+        return self.spark.createDataFrame(dataframe, schema)
+
+    def stop_spark_connection(self) -> None:
+        self.spark.stop()
+
+    def write_data_to_bucket(df: DataFrame, path: str, mode: str) -> None:
+        df.write.format("parquet").mode(mode).save(path)

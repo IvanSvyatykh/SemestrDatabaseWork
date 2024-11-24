767a6606a20ed0c5e4e8c6f7ee7ef16e14e74994
Ivan Svyatykh <zlativansvyat@mail.ru>
2024-11-24 15:35:30 +0500

    сгрупировал классы для получения данных вместе

--- staging_area/extract_data.py
+++ staging_area/extract_data.py
@@ -1,10 +1,11 @@
+import abc
 from pathlib import Path
+import pymongo
+from typing import List
+from pyspark.sql.types import StructType
 
 import sys
-from typing import List
-from numpy import spacing
-from pyspark.sql import DataFrame
-from data_workers import MongoDbExtractractor
+from pyspark.sql import SparkSession, DataFrame
 from spark_df_schemas import COLLECTIONS_SCHEMAS
 
 sys.path.append("..")
@@ -16,46 +17,97 @@ from config.config import (
 )
 
 
-def get_oid_from_spark_struct(df: DataFrame) -> DataFrame:
+class PySparkDataExtractor(abc.ABC):
 
-    columns = df.columns
+    @abc.abstractmethod
+    def start_spark_connection(
+        self, park_session_name: str, spark_ip: str = "localhost"
+    ):
+        pass
 
-    for column in columns:
-        print(df[column])
+    @property
+    @abc.abstractmethod
+    def db_objects(self) -> List[str]:
+        pass
+
+    @abc.abstractmethod
+    def stop_spark_connection(self):
+        pass
+
+    @abc.abstractmethod
+    def get_db_obj_df(
+        self, obj_name: str, obj_schema: StructType
+    ) -> DataFrame:
+        pass
 
 
-def get_data_from_db(
-    mongo_db_data_extractor: MongoDbExtractractor, path_to_parent_dir: str
-) -> List[Path]:
-    assert Path(path_to_parent_dir).exists()
-    results_paths = []
-    for collection in mongo_db_data_extractor.db_collections:
-        spark_df: DataFrame = (
-            mongo_db_data_extractor.get_df_from_collection(
+class MongoDbExtractractor(PySparkDataExtractor):
+
+    def __init__(
+        self,
+        db_username: str,
+        db_password: str,
+        db_domain: str,
+        db_name: str,
+        db_port: str = "27017",
+    ):
+
+        self.mongodb_uri = f"mongodb://{db_username}:{db_password}@{db_domain}:{db_port}/{db_name}?authSource=admin"
+        client = pymongo.MongoClient(self.mongodb_uri)
+        db = client[db_name]
+        self.__db_objects = db.list_collection_names()
+
+    @property
+    def db_objects(self) -> List[str]:
+        return self.__db_objects
+
+    def start_spark_connection(
+        self, spark_session_name: str, spark_ip: str = "localhost"
+    ) -> None:
+        self.spark: SparkSession = (
+            SparkSession.builder.appName(spark_session_name)
+            .master(f"spark://{spark_ip}:7077")
+            .config("spark.mongodb.input.uri", self.mongodb_uri)
+            .config(
+                "spark.jars.packages",
+                "org.mongodb.spark:mongo-spark-connector_2.12:3.0.1",
+            )
+            .getOrCreate()
+        )
+
+    def stop_spark_connection(self) -> None:
+        self.spark.stop()
+
+    def get_db_obj_df(
+        self, obj_name: str, obj_schema: StructType
+    ) -> DataFrame:
+        return (
+            self.spark.read.format("com.mongodb.spark.sql.DefaultSource")
+            .schema(obj_schema)
+            .option("collection", obj_name)
+            .load()
+        )
+
+
+class DataExtractor:
+
+    def __init__(
+        self, temp_dir_path: str, spark_extractor: PySparkDataExtractor
+    ):
+        assert Path(temp_dir_path).exists()
+        self.temp_dir_path = temp_dir_path
+        self.spark_extractor = spark_extractor
+
+    def get_data_from_db(self) -> List[Path]:
+
+        results_paths = []
+        for collection in self.spark_extractor.db_objects:
+            spark_df: DataFrame = self.spark_extractor.get_db_obj_df(
                 collection, COLLECTIONS_SCHEMAS[collection]
             )
-        )
-        path = path_to_parent_dir + f"/{collection}.csv.gz"
-        spark_df.toPandas().to_csv(path, index=False, compression="gzip")
-        results_paths.append(path)
-
-
-def main():
-    mongo_db_data_extractor = MongoDbExtractractor(
-        MONGODB_USERNAME,
-        MONGODB_PASSWORD,
-        MONGODB_DOMAIN,
-        "airport",
-        MONGODB_PORT,
-    )
-    mongo_db_data_extractor.start_spark_connection(
-        "ETL project", spark_ip="172.18.0.4"
-    )
-    result_paths = get_data_from_db(
-        mongo_db_data_extractor,
-        "/home/ivan/ЧелГУ/БД/Курсовая/Семестровая по ХД/data/spark_data",
-    )
-
-
-if __name__ == "__main__":
-    main()
+            path = self.temp_dir_path + f"/{collection}.csv.gz"
+            spark_df.toPandas().to_csv(
+                path, index=False, compression="gzip"
+            )
+            results_paths.append(path)
+        return results_paths

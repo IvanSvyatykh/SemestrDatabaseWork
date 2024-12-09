beae7f1d97413ebf13efce87a6425118c21cc5ef
Ivan Svyatykh <zlativansvyat@mail.ru>
2024-12-09 17:37:39 +0500

    доделал satelitte для загрузки

--- dags/utils/database/dwh_tables_scheamas.py
+++ dags/utils/database/dwh_tables_scheamas.py
@@ -528,3 +528,357 @@ class PassengerLink:
     @property
     def passenger_hash(self) -> str:
         return self.__passenger_hash
+
+
+class StatusInfosSat:
+    def __init__(
+        self,
+        load_date: datetime,
+        record_source: str,
+        status_info_hash: str,
+        set_status_time: datetime,
+        unset_status_time: datetime,
+    ):
+        self.__load_date = load_date
+        self.__record_source = record_source
+        self.__status_info_hash = status_info_hash
+        self.__set_status_time = set_status_time
+        self.__unset_status_time = unset_status_time
+
+    @property
+    def load_date(self) -> str:
+        return self.__load_date
+
+    @property
+    def record_source(self) -> str:
+        return self.__record_source
+
+    @property
+    def status_info_hash(self) -> str:
+        return self.__status_info_hash
+
+    @property
+    def set_status_time(self) -> str:
+        return self.__set_status_time
+
+    @property
+    def unset_status_time(self) -> str:
+        return self.__unset_status_time
+
+
+class SchedulesSat:
+    def __init__(
+        self,
+        load_date: datetime,
+        record_source: str,
+        schedules_hash: str,
+        actual_arrival_time: datetime,
+        actual_departure_time: datetime,
+        planned_arrival_time: datetime,
+        planned_departure_time: datetime,
+    ):
+        self.__load_date = load_date
+        self.__record_source = record_source
+        self.__schedules_hash = schedules_hash
+        self.__actual_arrival_time = actual_arrival_time
+        self.__actual_departure_time = actual_departure_time
+        self.__planned_arrival_time = planned_arrival_time
+        self.__planned_departure_time = planned_departure_time
+
+    @property
+    def load_date(self) -> str:
+        return self.__load_date
+
+    @property
+    def record_source(self) -> str:
+        return self.__record_source
+
+    @property
+    def schedules_hash(self) -> str:
+        return self.__schedules_hash
+
+    @property
+    def actual_arrival_time(self) -> datetime:
+        return self.__actual_arrival_time
+
+    @property
+    def actual_departure_time(self) -> datetime:
+        return self.__actual_departure_time
+
+    @property
+    def planned_arrival_time(self) -> datetime:
+        return self.__planned_arrival_time
+
+    @property
+    def planned_departure_time(self) -> datetime:
+        return self.__planned_departure_time
+
+
+class CargoFlightsSat:
+    def __init__(
+        self,
+        load_date: datetime,
+        record_source: str,
+        flight_hash: str,
+        weight: int,
+    ):
+        self.__load_date = load_date
+        self.__record_source = record_source
+        self.__flight_hash = flight_hash
+        self.__weight = weight
+
+    @property
+    def load_date(self) -> str:
+        return self.__load_date
+
+    @property
+    def record_source(self) -> str:
+        return self.__record_source
+
+    @property
+    def flight_hash(self) -> str:
+        return self.__flight_hash
+
+    @property
+    def weight(self) -> int:
+        return self.__weight
+
+
+class PassengerFlightsSat:
+    def __init__(
+        self,
+        load_date: datetime,
+        record_source: str,
+        flight_hash: str,
+        registration_time: datetime,
+        is_ramp: bool,
+        gate: str,
+    ):
+        self.__load_date = load_date
+        self.__record_source = record_source
+        self.__flight_hash = flight_hash
+        self.__registration_time = registration_time
+        self.__is_ramp = is_ramp
+        self.__gate = gate
+
+    @property
+    def load_date(self) -> str:
+        return self.__load_date
+
+    @property
+    def record_source(self) -> str:
+        return self.__record_source
+
+    @property
+    def flight_hash(self) -> str:
+        return self.__flight_hash
+
+    @property
+    def registration_time(self) -> datetime:
+        return self.__registration_time
+
+    @property
+    def is_ramp(self) -> bool:
+        return self.__is_ramp
+
+    @property
+    def gate(self) -> str:
+        return self.__gate
+
+
+class AirportsSat:
+    def __init__(
+        self,
+        load_date: datetime,
+        record_source: str,
+        airport_hash: str,
+        name: str,
+        city: str,
+        timezone: str,
+    ):
+        self.__load_date = load_date
+        self.__record_source = record_source
+        self.__airport_hash = airport_hash
+        self.__name = name
+        self.__city = city
+        self.__timezone = timezone
+
+    @property
+    def load_date(self) -> str:
+        return self.__load_date
+
+    @property
+    def record_source(self) -> str:
+        return self.__record_source
+
+    @property
+    def airport_hash(self) -> str:
+        return self.__airport_hash
+
+    @property
+    def name(self) -> str:
+        return self.__name
+
+    @property
+    def city(self) -> str:
+        return self.__city
+
+    @property
+    def timezone(self) -> str:
+        return self.__timezone
+
+
+class AircraftsSat:
+    def __init__(
+        self,
+        load_date: datetime,
+        record_source: str,
+        aircraft_hash: str,
+        name: str,
+        seats_num: int,
+    ):
+        self.__load_date = load_date
+        self.__record_source = record_source
+        self.__aircraft_hash = aircraft_hash
+        self.__name = name
+        self.__seats_num = seats_num
+
+    @property
+    def load_date(self) -> str:
+        return self.__load_date
+
+    @property
+    def record_source(self) -> str:
+        return self.__record_source
+
+    @property
+    def aircraft_hash(self) -> str:
+        return self.__aircraft_hash
+
+    @property
+    def name(self) -> str:
+        return self.__name
+
+    @property
+    def seats_num(self) -> str:
+        return self.__seats_num
+
+
+class AircraftNumsSat:
+    def __init__(
+        self,
+        load_date: datetime,
+        record_source: str,
+        aircraft_num_hash: str,
+        registration_time: datetime,
+        deregistration_time: datetime,
+    ):
+        self.__load_date = load_date
+        self.__record_source = record_source
+        self.__aircraft_num_hash = aircraft_num_hash
+        self.__registration_time = registration_time
+        self.__deregistration_time = deregistration_time
+
+    @property
+    def load_date(self) -> str:
+        return self.__load_date
+
+    @property
+    def record_source(self) -> str:
+        return self.__record_source
+
+    @property
+    def aircraft_num_hash(self) -> str:
+        return self.__aircraft_num_hash
+
+    @property
+    def registration_time(self) -> datetime:
+        return self.__registration_time
+
+    @property
+    def deregistration_time(self) -> datetime:
+        return self.__deregistration_time
+
+
+class TicketsSat:
+    def __init__(
+        self,
+        load_date: datetime,
+        record_source: str,
+        ticket_hash: str,
+        cost: float,
+        baggage_weight: float,
+        is_registred: bool,
+        seat_num: str,
+    ):
+        self.__load_date = load_date
+        self.__record_source = record_source
+        self.__ticket_hash = ticket_hash
+        self.__cost = cost
+        self.__baggage_weight = baggage_weight
+        self.__is_registred = is_registred
+        self.__seat_num = seat_num
+
+    @property
+    def load_date(self) -> str:
+        return self.__load_date
+
+    @property
+    def record_source(self) -> str:
+        return self.__record_source
+
+    @property
+    def ticket_hash(self) -> str:
+        return self.__ticket_hash
+
+    @property
+    def cost(self) -> datetime:
+        return self.__cost
+
+    @property
+    def baggage_weight(self) -> datetime:
+        return self.__baggage_weight
+
+    @property
+    def is_registred(self) -> datetime:
+        return self.__is_registred
+
+    @property
+    def seat_num(self) -> datetime:
+        return self.__seat_num
+
+
+class PassengersSat:
+    def __init__(
+        self,
+        load_date: datetime,
+        record_source: str,
+        passenger_hash: str,
+        name: str,
+        surname: str,
+    ):
+        self.__load_date = load_date
+        self.__record_source = record_source
+        self.__passenger_hash = passenger_hash
+        self.__name = name
+        self.__surname = surname
+
+    @property
+    def load_date(self) -> str:
+        return self.__load_date
+
+    @property
+    def record_source(self) -> str:
+        return self.__record_source
+
+    @property
+    def passenger_hash(self) -> str:
+        return self.__passenger_hash
+
+    @property
+    def name(self) -> str:
+        return self.__name
+
+    @property
+    def surname(self) -> str:
+        return self.__surname
--- dags/utils/database/repositories.py
+++ dags/utils/database/repositories.py
@@ -1,21 +1,30 @@
 from dwh_tables_scheamas import (
     AircraftNumsLink,
+    AircraftNumsSat,
     AircraftsHub,
     AircraftsLink,
+    AircraftsSat,
     AirlinesHub,
     AirportsHub,
     AirportsLink,
+    AirportsSat,
+    CargoFlightsSat,
+    PassengerFlightsSat,
     PassengerLink,
     PassengersHub,
     SchedulesLink,
+    SchedulesSat,
     SeatClassesHub,
     SeatClassesLink,
     StatusHub,
     SchedulesHub,
     FlightsHub,
+    StatusInfosSat,
     StatusesInfosLink,
     TicketsHub,
     TicketsLink,
+    PassengersSat,
+    TicketsSat,
 )
 
 
@@ -378,4 +387,221 @@ class PassengersLinkRepositoty:
                 load_date timestamp WITH TIME ZONE,
                 record_source varchar(150),
                 passenger_hash_key varchar(32));
+            """
+
+
+class StatusInfosSatRepositoty:
+
+    __name = "status_infos_sat"
+
+    def insert(self, status_info_sat: StatusInfosSat) -> str:
+        return f"""INSERT INTO {self.__name} (status_info_hash_key, load_date, record_source,set_status_time,unset_status_time)
+                VALUES ({status_info_sat.status_info_hash}, 
+                {status_info_sat.load_date}, 
+                {status_info_sat.record_source},
+                {status_info_sat.set_status_time},
+                {status_info_sat.unset_status_time});
+                """
+
+    def create_table(self) -> str:
+        return f""" CREATE TABLE IF NOT EXISTS {self.__name} (
+                status_info_hash_key varchar(32),
+                load_date timestamp WITH TIME ZONE,
+                record_source varchar(150),
+                set_status_time timestamp WITH TIME ZONE,
+                unset_status_time timestamp WITH TIME ZONE);
+            """
+
+
+class SchedulesSatRepositoty:
+
+    __name = "schedules_sat"
+
+    def insert(self, schedule_sat: SchedulesSat) -> str:
+        return f"""INSERT INTO {self.__name} (schedules_hash_key, load_date, record_source,actual_arrival_time,actual_departure_time,planned_arrival_time,planned_departure_time)
+                VALUES ({schedule_sat.schedules_hash}, 
+                {schedule_sat.load_date}, 
+                {schedule_sat.record_source},
+                {schedule_sat.actual_arrival_time},
+                {schedule_sat.actual_departure_time},
+                {schedule_sat.planned_arrival_time},
+                {schedule_sat.planned_departure_time},);
+                """
+
+    def create_table(self) -> str:
+        return f""" CREATE TABLE IF NOT EXISTS {self.__name} (
+                schedules_hash_key varchar(32),
+                load_date timestamp WITH TIME ZONE,
+                record_source varchar(150),
+                actual_arrival_time timestamp WITH TIME ZONE,
+                actual_departure_time timestamp WITH TIME ZONE,
+                planned_arrival_time timestamp WITH TIME ZONE,
+                planned_departure_time timestamp WITH TIME ZONE);
+            """
+
+
+class CargoFlightsSatRepositoty:
+
+    __name = "cargo_flights_sat"
+
+    def insert(self, cargo_sat: CargoFlightsSat) -> str:
+        return f"""INSERT INTO {self.__name} (flight_hash_key, load_date, record_source,weight)
+                VALUES ({cargo_sat.flight_hash}, 
+                {cargo_sat.load_date}, 
+                {cargo_sat.record_source},
+                {cargo_sat.weight},);
+                """
+
+    def create_table(self) -> str:
+        return f""" CREATE TABLE IF NOT EXISTS {self.__name} (
+                flight_hash_key varchar(32),
+                load_date timestamp WITH TIME ZONE,
+                record_source varchar(150),
+                weight INTEGER,);
+            """
+
+
+class PassengerFlightsSatRepositoty:
+
+    __name = "passenger_flights_sat"
+
+    def insert(self, passenger_sat: PassengerFlightsSat) -> str:
+        return f"""INSERT INTO {self.__name} (flight_hash_key, load_date, record_source,registration_time,is_ramp,gate)
+                VALUES ({passenger_sat.flight_hash}, 
+                {passenger_sat.load_date}, 
+                {passenger_sat.record_source},
+                {passenger_sat.registration_time},
+                {passenger_sat.is_ramp},
+                {passenger_sat.gate});
+                """
+
+    def create_table(self) -> str:
+        return f""" CREATE TABLE IF NOT EXISTS {self.__name} (
+                flight_hash_key varchar(32),
+                load_date timestamp WITH TIME ZONE,
+                record_source varchar(150),
+                registration_time timestamp WITH TIME ZONE,
+                is_ramp BOOLEAN,
+                gate varchar(3));
+            """
+
+
+class AirportsSatRepositoty:
+
+    __name = "airports_sat"
+
+    def insert(self, airport_sat: AirportsSat) -> str:
+        return f"""INSERT INTO {self.__name} (airport_hash_key, load_date, record_source,name,city,timezone)
+                VALUES ({airport_sat.airport_hash}, 
+                {airport_sat.load_date}, 
+                {airport_sat.record_source},
+                {airport_sat.name},
+                {airport_sat.city},
+                {airport_sat.timezone});
+                """
+
+    def create_table(self) -> str:
+        return f""" CREATE TABLE IF NOT EXISTS {self.__name} (
+                airport_hash_key varchar(32),
+                load_date timestamp WITH TIME ZONE,
+                record_source varchar(150),
+                name varchar(50),
+                city varchar(50),
+                timezone varchar(50));
+            """
+
+
+class AircraftsSatRepositoty:
+
+    __name = "aircrafts_sat"
+
+    def insert(self, aircraft_sat: AircraftsSat) -> str:
+        return f"""INSERT INTO {self.__name} (aircraft_hash_key, load_date, record_source,name,seats_num)
+                VALUES ({aircraft_sat.aircraft_hash}, 
+                {aircraft_sat.load_date}, 
+                {aircraft_sat.record_source},
+                {aircraft_sat.name},
+                {aircraft_sat.seats_num});
+                """
+
+    def create_table(self) -> str:
+        return f""" CREATE TABLE IF NOT EXISTS {self.__name} (
+                aircraft_hash_key varchar(32),
+                load_date timestamp WITH TIME ZONE,
+                record_source varchar(150),
+                name varchar(50),
+                seats_num INTEGER);
+            """
+
+
+class AircraftNumsSatRepositoty:
+
+    __name = "aircraft_nums_sat"
+
+    def insert(self, aircraft_num_sat: AircraftNumsSat) -> str:
+        return f"""INSERT INTO {self.__name} (aircraft_hash_key, load_date, record_source,registration_time,deregistration_time)
+                VALUES ({aircraft_num_sat.aircraft_num_hash}, 
+                {aircraft_num_sat.load_date}, 
+                {aircraft_num_sat.record_source},
+                {aircraft_num_sat.registration_time},
+                {aircraft_num_sat.deregistration_time});
+                """
+
+    def create_table(self) -> str:
+        return f""" CREATE TABLE IF NOT EXISTS {self.__name} (
+                aircraft_num_hash_key varchar(32),
+                load_date timestamp WITH TIME ZONE,
+                record_source varchar(150),
+                registration_time timestamp WITH TIME ZONE,
+                deregistration_time timestamp WITH TIME ZONE);
+            """
+
+
+class TicketsSatRepositoty:
+
+    __name = "tickets_sat"
+
+    def insert(self, ticket_sat: TicketsSat) -> str:
+        return f"""INSERT INTO {self.__name} (ticket_hash_key, load_date, record_source,cost,baggage_weight,is_registred,seat_num)
+                VALUES ({ticket_sat.ticket_hash}, 
+                {ticket_sat.load_date}, 
+                {ticket_sat.record_source},
+                {ticket_sat.cost},
+                {ticket_sat.baggage_weight},
+                {ticket_sat.is_registred},
+                {ticket_sat.seat_num});
+                """
+
+    def create_table(self) -> str:
+        return f""" CREATE TABLE IF NOT EXISTS {self.__name} (
+                ticket_hash_key varchar(32),
+                load_date timestamp WITH TIME ZONE,
+                record_source varchar(150),
+                cost FLOAT,
+                baggage_weight FLOAT,
+                is_registred BOOLEAN, 
+                seat_num varchar(3));
+            """
+
+
+class PassengersSatRepositoty:
+
+    __name = "passengers_sat"
+
+    def insert(self, passenger_sat: PassengersSat) -> str:
+        return f"""INSERT INTO {self.__name} (passenger_hash_key, load_date, record_source,name,surname)
+                VALUES ({passenger_sat.passenger_hash}, 
+                {passenger_sat.load_date}, 
+                {passenger_sat.record_source},
+                {passenger_sat.name},
+                {passenger_sat.surname});
+                """
+
+    def create_table(self) -> str:
+        return f""" CREATE TABLE IF NOT EXISTS {self.__name} (
+                passenger_hash_key varchar(32),
+                load_date timestamp WITH TIME ZONE,
+                record_source varchar(150),
+                name varchar(50), 
+                surname varchar(50));
             """
